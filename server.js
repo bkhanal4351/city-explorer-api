62fc81fd9a701c9cfa421ff9edf5da8c3e7e4fc1
Bishal Khanal <bishal.khanal87@gmail.com>
2022-03-24 00:38:04 -0700

    added movies backend url

--- server.js
+++ server.js
@@ -40,9 +40,23 @@ app.get('/weather', async (request, response) => {
 
 });
 
+app.get('/movies', async (request, response) => {
+  let city = request.query.city_name;
+  let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&city=${city}&total_results=3`;
+  let movieResult = await axios.get(url);
+  console.log(url);
+  let movieData = new Movie(movieResult.data);
+
+
+
+
+  response.send(movieData);
+
+});
+
 
 app.get('*', (request, response) => {
-  response.send('some message');
+  response.send('Error received! Try again!');
 });
 
 
@@ -52,6 +66,15 @@ class Forecast {
   constructor(cityData) {
     this.date = cityData.data[0].ob_time;
     this.description = cityData.data[0].weather.description;
+  }
+}
+
+class Movie {
+  constructor(value) {
+    this.title= value.title;
+    this.description = value.overview;
+    this.language = value.original_language;
+    this.tagline= value.tagline;
   }
 }
 
