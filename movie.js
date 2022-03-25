8165a5cc56bb41649dc8796864d562891cacd439
Bishal Khanal <bishal.khanal87@gmail.com>
2022-03-24 17:58:13 -0700

    completed all features

--- movie.js
+++ movie.js
@@ -5,10 +5,13 @@ const axios = require('axios');
 async function getMovies(request, response) {
   let city = request.query.city_name;
 
-  let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${city}&total_results=7`;
+  let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API}&query=${city}&total_results=4`;
   let movieResult = await axios.get(url);
-  console.log(url);
-  let movieData = new Movie(movieResult.data);
+  console.log(movieResult);
+  // let movieData = new Movie(movieResult.data);
+  let movieData = movieResult.data.results.map(movie => {
+    return new Movie(movie);
+  });
 
   console.log(movieData.data);
 
@@ -20,24 +23,25 @@ async function getMovies(request, response) {
 
 
 
-
+// this.moviesParsed = [];
+// movieValue.results.forEach(movie => {
+//   this.moviesParsed.push({
 class Movie {
   constructor(movieValue) {
-    this.moviesParsed = [];
-    movieValue.results.forEach(movie => {
-      this.moviesParsed.push({
-        title: movie.original_title,
-        overview: movie.overview,
-        vote_average: movie.vote_average,
-        vote_count: movie.vote_count,
-        img: movie.poster_path,
-        popularity: movie.popularity,
-        release_date: movie.release_date
-      });
-    });
 
+
+    this.title = movieValue.original_title;
+    this.overview = movieValue.overview;
+    this.vote_average = movieValue.vote_average;
+    this.vote_count = movieValue.vote_count;
+    // this.img= movieValue.poster_path;
+    this.popularity = movieValue.popularity;
+    this.release_date = movieValue.release_date;
   }
+
+
 }
+
 
 
 module.exports = getMovies;
