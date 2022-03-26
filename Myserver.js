bae855537cc3ea1b631d504429ff64d06d68d815
Bishal Khanal <bishal.khanal87@gmail.com>
2022-03-25 22:44:19 -0700

    started lab10

---
+++ Myserver.js
@@ -0,0 +1,66 @@
+'use strict';
+
+
+console.log('My first server');
+
+
+const express = require('express');
+require('dotenv').config();
+// let weatherData = require('./data/weather.json');
+
+const cors = require('cors');
+const getForecast = require('./weather.js');
+
+
+
+
+
+const app = express();
+app.use(cors());
+
+const getMovies = require('./movie.js');
+
+
+const PORT = process.env.PORT || 3002;
+
+
+app.get('/', (request, response) => {
+  response.send('Hello');
+});
+
+app.get('/weather', getForecast);
+// let city = request.query.city_name;
+// let url = `https://api.weatherbit.io/v2.0/current?city=${city}&key=${process.env.WEATHER_API}`;
+// let weatherResult = await axios.get(url);
+// console.log(url);
+// let forecastData = new Forecast(weatherResult.data);
+
+
+
+
+// response.send(forecastData);
+
+
+
+app.get('/movie', getMovies);
+
+
+
+
+
+
+
+
+// class Forecast {
+//   constructor(cityData) {
+//     this.date = cityData.data[0].ob_time;
+//     this.description = cityData.data[0].weather.description;
+//   }
+// }
+
+
+
+// LISTEN
+// start the server
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
--- server.js
+++ server.js
@@ -1,66 +1,22 @@
 'use strict';
 
-
-console.log('My first server');
-
-
+require('dotenv');
 const express = require('express');
-require('dotenv').config();
-// let weatherData = require('./data/weather.json');
-
 const cors = require('cors');
-const getForecast = require('./weather.js');
 
-
-
-
-
+const weather = require('./modules/weather.js');
 const app = express();
-app.use(cors());
 
-const getMovies = require('./movie.js');
+app.get('/weather', weatherHandler);
 
+function weatherHandler(request, response) {
+  const { lat, lon } = request.query;
+  weather(lat, lon)
+  .then(summaries => response.send(summaries))
+  .catch((error) => {
+    console.error(error);
+    response.status(200).send('Sorry. Something went wrong!')
+  });
+}  
 
-const PORT = process.env.PORT || 3002;
-
-
-app.get('/', (request, response) => {
-  response.send('Hello');
-});
-
-app.get('/weather', getForecast);
-// let city = request.query.city_name;
-// let url = `https://api.weatherbit.io/v2.0/current?city=${city}&key=${process.env.WEATHER_API}`;
-// let weatherResult = await axios.get(url);
-// console.log(url);
-// let forecastData = new Forecast(weatherResult.data);
-
-
-
-
-// response.send(forecastData);
-
-
-
-app.get('/movie', getMovies);
-
-
-
-
-
-
-
-
-// class Forecast {
-//   constructor(cityData) {
-//     this.date = cityData.data[0].ob_time;
-//     this.description = cityData.data[0].weather.description;
-//   }
-// }
-
-
-
-// LISTEN
-// start the server
-
-app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+app.listen(process.env.PORT, () => console.log(`Server up on ${process.env.PORT}`));
